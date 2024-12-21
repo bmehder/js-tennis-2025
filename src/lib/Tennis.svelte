740bd7ac4bab2f1254f8bbf86f2e5030c2070d6c
bmehder <bmehder@gmail.com>
2024-12-21 00:35:06 +0000

    score Set

--- src/lib/Tennis.svelte
+++ src/lib/Tennis.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
 	type Player = 0 | 1
 
+	type CurrentSet = 0 | 1 | 2
+
 	type Point = 0 | 15 | 30 | 40 | 'Ad'
 
 	type Game = [Point, Point]
 
-	type Set = [Game, Game]
-
-	type Match = {
-		set1: Game
-		set2: Game
-		set3: Game
-	}
+	type Set = [number, number]
 
 	const pointScoringMap = new Map<Point, Point>([
 		[0, 15],
@@ -20,47 +16,86 @@
 		[40, 0],
 	])
 
-	const points: Game = $state([0, 0])
+	// State
+	const point: Game = $state([0, 0])
+
+	const currentSet: CurrentSet = $state(0)
+
+	const sets: Set[] = $state([
+		[0, 0],
+		[0, 0],
+		[0, 0],
+	])
+
+	const isDeuce = $derived(point.every(score => score === 40))
 
 	const resetGame = () => {
-		points[0] = 0
-		points[1] = 0
+		point[0] = 0
+		point[1] = 0
 	}
 
+	const deuce = () => {
+		point[0] = 40
+		point[1] = 40
+	}
+
+	// State mutators
 	const scorePoint = (winner: Player) => {
 		const loser = winner === 0 ? 1 : 0
 
-		if (points[winner] === 'Ad') {
+		if (point[winner] === 'Ad') {
 			resetGame()
+			sets[currentSet][winner]++
 			return
 		}
 
-		if (points[loser] === 'Ad') {
-			points[winner] = 40
-			points[loser] = 40
+		if (point[loser] === 'Ad') {
+			deuce()
 			return
 		}
 
-		if (points.every(point => point === 40)) {
-			points[winner] = 'Ad'
+		if (isDeuce) {
+			point[winner] = 'Ad'
 			return
 		}
 
-		points[winner] = pointScoringMap.get(points[winner]) as Point
+		point[winner] = pointScoringMap.get(point[winner]) as Point
+
+		if (point[winner] === 0) {
+			sets[currentSet][winner]++
+			resetGame()
+		}
 	}
 
-	const handleClick = (player: Player) => {
-		scorePoint(player)
+	const handleClick = (winner: Player) => {
+		scorePoint(winner)
 	}
 </script>
 
-<div>
-	{points[0]}
-</div>
+<div class="scoreboard">
+	{#each sets as set}
+		<div>
+			<div>{set[0]}</div>
+			<div>{set[1]}</div>
+		</div>
+	{/each}
 
-<div>
-	{points[1]}
+	<div>
+		<div>
+			{point[0]}
+		</div>
+		<div>
+			{point[1]}
+		</div>
+	</div>
 </div>
 
 <button onclick={() => handleClick(0)}> Player 1 </button>
 <button onclick={() => handleClick(1)}> Player 2 </button>
+
+<style>
+	.scoreboard {
+		display: flex;
+		gap: 1rem;
+	}
+</style>
